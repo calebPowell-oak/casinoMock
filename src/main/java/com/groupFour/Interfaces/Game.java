2fd6573087749dad1151250bb4210cc3d5f4888c
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-06-11 16:57:37 -0400

    Final uml adjustments

--- src/main/java/com/groupFour/Interfaces/Game.java
+++ src/main/java/com/groupFour/Interfaces/Game.java
@@ -1,10 +1,14 @@
 package com.groupFour.Interfaces;
 
 public abstract class Game {
+
+    private Boolean donePlaying;
+
     public abstract void takeTurn();
 
-    public void exit(){
 
+    public void exit(){
+        // flip done playing
     }
 
     public abstract void setup();
